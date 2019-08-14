731a9a99b7a7a36146e1325cba43794e4be1d962
Kris De Volder <kdevolder@pivotal.io>
2019-08-14 16:51:08 -0700

    Initial commit: adopt circles examples
    - example is now standalone client.
    - no server yet. Just uses local model provider in the client.
    
    Signed-off-by: Kris De Volder <kdevolder@pivotal.io>

---
+++ client/src/di.config.ts
@@ -0,0 +1,51 @@
+/********************************************************************************
+ * Copyright (c) 2017-2018 TypeFox and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+console.log("Loading di.config");
+import "reflect-metadata"
+
+import { Container, ContainerModule } from "inversify";
+import {
+    defaultModule, TYPES, configureViewerOptions, SGraphView, PolylineEdgeView, ConsoleLogger,
+    LogLevel, WebSocketDiagramServer, boundsModule, moveModule, selectModule, undoRedoModule, viewportModule,
+    LocalModelSource, exportModule, CircularNode, configureModelElement, SGraph, SEdge, updateModule,
+    graphModule, routingModule, modelSourceModule
+} from "sprotty";
+import { CircleNodeView } from "./views";
+
+export default (useWebsocket: boolean) => {
+    require("sprotty/css/sprotty.css");
+    require("../css/diagram.css");
+    const circlegraphModule = new ContainerModule((bind, unbind, isBound, rebind) => {
+        if (useWebsocket)
+            bind(TYPES.ModelSource).to(WebSocketDiagramServer).inSingletonScope();
+        else
+            bind(TYPES.ModelSource).to(LocalModelSource).inSingletonScope();
+        rebind(TYPES.ILogger).to(ConsoleLogger).inSingletonScope();
+        rebind(TYPES.LogLevel).toConstantValue(LogLevel.log);
+        const context = { bind, unbind, isBound, rebind };
+        configureModelElement(context, 'graph', SGraph, SGraphView);
+        configureModelElement(context, 'node:circle', CircularNode, CircleNodeView);
+        configureModelElement(context, 'edge:straight', SEdge, PolylineEdgeView);
+        configureViewerOptions(context, {
+            needsClientLayout: false
+        });
+    });
+
+    const container = new Container();
+    container.load(defaultModule, selectModule, moveModule, boundsModule, undoRedoModule, viewportModule,
+        exportModule, updateModule, graphModule, routingModule, modelSourceModule, circlegraphModule);
+    return container;
+};
