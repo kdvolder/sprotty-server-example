5a439fe39d1952ac8d6875a149884ff76fb9e3bf
Kris De Volder <kdevolder@pivotal.io>
2019-08-15 11:53:45 -0700

    Add copy of chatter-web-ui
    Signed-off-by: Kris De Volder <kdevolder@pivotal.io>

---
+++ server/src/main/resources/static/chat-app.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+	var MAX_HISTORY = 15;
+	
+	var isPWS = false; //(window.location.hostname.indexOf("cfapps.io") > -1);
+	
+	var ws = new SockJS(isPWS
+			?'https://'+window.location.hostname+':4443/websocket'
+			:'/websocket'
+	);
+	
+	// Wire up websocket so that msg received from it are shown in the 'console'.
+	ws.onopen = function () {
+		log('inf: WebSocket connection opened.');
+	};
+	ws.onerror = function (error) {
+		log('err: '+error);
+	}
+	ws.onmessage = function (event) {
+		log(event.data);
+	};
+	ws.onclose = function () {
+		log('inf: WebSocket connection closed.');
+	};
+
+	$("#msg").keyup(function(e){
+	    if(e.keyCode == 13) {
+	        $("#send").trigger("click");
+	    }
+	})
+	$("#send").click(function () {
+		var msg = $("#msg").val();
+		ws.send(msg);
+	//	log('Sent: '+msg);
+	});
+	
+	function log(message) {
+		var msgs = $("#messages");
+		var newMsg = $('<li>').text(message)
+		msgs.append(newMsg);
+		newMsg.get(0).scrollIntoView();
+		while (msgs.scrollTop()>0) {
+			$("#messages").children()[0].remove();
+		}
+	}
+
+})
